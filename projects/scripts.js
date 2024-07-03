7a50b53c39892a818ffdc1462a429dac591bd638
Ashmita001 <ashmita.202004@gmail.com>
2024-07-04 04:38:37 +0530

    cart

--- projects/scripts.js
+++ projects/scripts.js
@@ -1,42 +1,49 @@
 
 // data structure
-const products = [{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars: 4.5,
-        count: 87
-    },
-    priceCents: 1090
-}, {
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-        stars: 4,
-        count: 127
-    },
-    priceCents: 2095
+// const products = [{
+//     image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+//     name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+//     rating: {
+//         stars: 4.5,
+//         count: 87
+//     },
+//     priceCents: 1090
+// }, {
+//     image: 'images/products/intermediate-composite-basketball.jpg',
+//     name: 'Intermediate Size Basketball',
+//     rating: {
+//         stars: 4,
+//         count: 127
+//     },
+//     priceCents: 2095
 
-}, {
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: ' Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 4.5,
-        count: 56
-    },
-    priceCents: 799
-},{
-    
-}];
+// }, {
+//     image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+//     name: ' Adults Plain Cotton T-Shirt - 2 Pack',
+//     rating: {
+//         stars: 4.5,
+//         count: 56
+//     },
+//     priceCents: 799
+// },{
+//     image:'images/products/black-2-slot-toaster.jpg',
+//     name:'2 Slot Toaster - Black',
+//     rating:{
+//       stars:5,
+//       count:2197
+//     },
+//     priceCents:1899
+
+// }];
 
 
-let productsHTML=''
+let productsHTML = ''
 // generating html
 products.forEach((product) => {
-    // const html = `
+  // const html = `
 
-    
-    productsHTML+=`
+
+  productsHTML += `
     <div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
@@ -50,14 +57,14 @@ products.forEach((product) => {
         
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/rating-${product.rating.stars*10}.png">
+              src="images/ratings/rating-${product.rating.stars * 10}.png">
             <div class="product-rating-count link-primary">
             ${product.rating.count}
             </div>
           </div>
 
           <div class="product-price">
-            $${(product.priceCents/100).toFixed(2)}
+            $${(product.priceCents / 100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
@@ -82,18 +89,54 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id
+    }">
             Add to Cart
           </button>
         </div>
     `;
-    // console.log(html);
-    
+  // console.log(html);
+
 });
 
-console.log(productsHTML);
+// console.log(productsHTML);
 
-document.querySelector('.js-products-grid').innerHTML=productsHTML;
+document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
-// now we can delete all the code in the html file and replace it with js code
+// now we can delete all the code in the html file and replace it with js code---upto now
+
+
+// add to cart button 
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+  button.addEventListener('click', () => {
+    const productId = button.dataset.productId;
+
+    let matchingItem;
+
+    cart.forEach((item) => {
+      if (productId === item.productId) {
+        matchingItem = item;
+      }
+    });
+
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      cart.push({
+        productId: productId,
+        quantity: 1
+      });
+
+    }
+
+
+
+    console.log(cart)
+  });
+
+});
+
+
+
 
